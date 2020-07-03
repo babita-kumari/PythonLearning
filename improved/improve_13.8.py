e6618490d9b73538f86f547e6f790add3adb67a6
Dr. Babita Kumari <babita.kumari.april1992@gmail.com>
2020-07-04 00:22:35 +0530

    improve assignment

---
+++ improved/improve_13.8.py
@@ -0,0 +1,64 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+
+
+
+#http://py4e-data.dr-chuck.net/json?address=xyz&key=42
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+class FindAddress:
+
+    def __init__(self):
+        self.api_key = str(42)
+        self.serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+
+    def get_input(self,api_key):
+        address="bangalore"
+        new_address={}
+        new_address['address']=address
+        new_address['key']=api_key
+        return new_address
+
+    def read_url(self,new_address,serviceurl):
+        url = serviceurl + urllib.parse.urlencode(new_address)
+        uh = urllib.request.urlopen(url, context=ctx)
+        data = uh.read()
+        data=data.decode()
+        print(data)
+        return data
+
+    def load_data(self,data):
+        js=json.loads(data)
+        print(js)
+        return js
+
+    def find_place_id(self,js):
+        place_id=[]
+        for place in js["results"]:
+            place_id=place[ "place_id"]
+            return place_id
+
+    def print_result(self,place_id):
+        print(place_id)
+
+    def processes(self):
+        input=self.get_input(self.api_key);
+        url=self.read_url(input,self.serviceurl);
+        data=self.load_data(url);
+        place_id=self.find_place_id(data);
+        self.print_result(place_id)
+
+place_dict=FindAddress()
+place_dict.processes()
+
+
+
+
+
+
+
---
+++ improved/improve_15.2.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+conn = sqlite3.connect('new_emaildb1.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+class SqlDatabaseCreation:
+
+    def get_input(self):
+        fname = 'mbox.txt'
+        if (len(fname) < 1): fname = 'mbox.txt'
+        fh = open(fname)
+        organisation=[]
+        for line in fh:
+            if not line.startswith('From: '): continue
+            pieces = line.split()
+            email = pieces[1]
+            new_pieces=email.find('@')
+            org=email[new_pieces+1:]
+            organisation.append(org)
+        return organisation
+
+    def execute_sql(self,organisation):
+        for org in organisation:
+            cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+            row = cur.fetchone()
+            if row is None:
+                cur.execute('''INSERT INTO Counts (org, count)
+                        VALUES (?, 1)''', (org,))
+            else:
+                cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                                (org,))
+            conn.commit()
+        return conn
+
+    def print_result(self):
+#https://www.sqlite.org/lang_select.html
+        sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+        for row in cur.execute(sqlstr):
+            print(str(row[0]), row[1])
+        cur.close()
+
+    def processes(self):
+        input=self.get_input();
+        sql_database=self.execute_sql(input);
+        self.print_result()
+
+sql_dataset_1=SqlDatabaseCreation()
+sql_dataset_1.processes()
