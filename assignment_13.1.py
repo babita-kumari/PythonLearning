33d2c93c925788d9c2b7c20493ea4c10356c8e57
Dr. Babita Kumari <babita.kumari.april1992@gmail.com>
2020-07-03 20:42:37 +0530

    modified assignment 13.2.py

--- assignment_13.1.py
+++ assignment_13.1.py
@@ -7,7 +7,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+url = " http://py4e-data.dr-chuck.net/comments_680098.html"
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
---
+++ improved/improve_13.1.py
@@ -0,0 +1,71 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+        # Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+class ParseHtmlFile:
+
+    def get_input(self):
+        url = " http://py4e-data.dr-chuck.net/comments_680098.html"
+        html = urlopen(url, context=ctx).read()
+        soup = BeautifulSoup(html,"html.parser")
+        return soup
+
+    def get_data(self,soup):
+        tag_list=[]
+        tags=soup('span')
+        for tag in tags:
+            tag = tag.contents[0]
+            tag_new = str(tag)
+            int_tag = int(tag_new)
+            tag_list.append(int_tag)
+        return tag_list
+
+    def get_sum(self,tag_list):
+        sum=0
+        for number in tag_list:
+            sum=sum+number
+        return sum
+
+    def get_count(self,tag_list):
+        count=len(tag_list)
+        return count
+
+    def print_result(self,sum,count):
+        print(sum,count)
+
+    def processes(self):
+        input=self.get_input();
+        data=self.get_data(input);
+        sum=self.get_sum(data);
+        count=self.get_count(data);
+        self.print_result(sum,count)
+
+data_bank=ParseHtmlFile()
+data_bank.processes()
+
+
+
+
+# Retrieve all of the anchor tags
+sum=0
+count=0
+tag_list=list()
+tags = soup('span')
+for tag in tags:
+    tag=tag.contents[0]
+    tag_new=str(tag)
+    count=count+1
+    int_tag=int(tag_new)
+    sum=sum+int_tag
+print(sum,count)
+
+#for i in tag_list:
+    #sum=sum+i
+    #count=count+1
+#print(sum,count)
+
